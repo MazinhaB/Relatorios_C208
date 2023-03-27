befaa1840e6e88bc6701eaddeb3448a983bc228b
MazinhaB <mariana.barude@gec.inatel.br>
2023-03-27 20:34:49 -0300

    Exercicio 1

---
+++ Relatorio3/Exercicio1.asm
@@ -0,0 +1,21 @@
+.text
+
+# Operação: 2 + 5
+li $t0, 2
+addi $t1, $t0, 5
+
+# Operação: 89 + 20
+li $t0, 89
+addi $t2, $t0, 20
+
+# Operação: -80 + 1234
+li $t0, 1234
+subi $t3, $t0, 80
+
+# Operação: 70 - 24
+li $t0, 70
+addi $t4, $t0, -24
+
+# Operação: 20 - 150
+li $t0, 20
+addi $t5, $t0, -150
